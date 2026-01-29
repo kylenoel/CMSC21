b7dea4e076da64f21acb3863c6b96d5489f4265c
kylenoel <khnoel@up.edu.ph>
2026-01-29 14:35:04 +0800

    Lecture demo, January 29, 2026

--- 01_c_basics.c
+++ 01_c_basics.c
@@ -11,54 +11,54 @@ we want to review the basics of programming in C:
 
 int main() {
     //variable declaration and initialization
-    // int age = 0;
-    // float height = 0.0;
-    // char grade = ' ';
+    int age = 0;
+    float height = 0.0;
+    char grade = ' ';
 
-    // printf("Enter your age: ");
-    // scanf("%d", &age);
+    printf("Enter your age: ");
+    scanf("%d", &age);
 
-    // printf("Enter your height in meters (e.g., 1.75): ");
-    // scanf("%f", &height);
+    printf("Enter your height in meters (e.g., 1.75): ");
+    scanf("%f", &height);
 
-    // printf("Enter your grade (A, B, C, D, F): ");
-    // scanf(" %c", &grade);
+    printf("Enter your grade (A, B, C, D, F): ");
+    scanf(" %c", &grade);
 
-    // //printing output to the console
-    // printf("== User Information ==\n");
-    // printf("Age: %d years\n", age);
-    // printf("Height: %.2f meters\n", height);
-    // printf("Grade: %c\n", grade);
+    //printing output to the console
+    printf("== User Information ==\n");
+    printf("Age: %d years\n", age);
+    printf("Height: %.2f meters\n", height);
+    printf("Grade: %c\n", grade);
 
-    // //if else conditional statement: minor/adult/senior citizen
-    // if(age < 18){
-    //     printf("You are a minor.\n");
-    // } else if(age >= 18 && age < 60){
-    //     printf("You are an adult.\n");
-    // } else {
-    //     printf("You are a senior citizen.\n");
-    // }
+    //if else conditional statement: minor/adult/senior citizen
+    if(age < 18){
+        printf("You are a minor.\n");
+    } else if(age >= 18 && age < 60){
+        printf("You are an adult.\n");
+    } else {
+        printf("You are a senior citizen.\n");
+    }
 
-    // //switch case: grade interpretation
-    // switch(grade){
-    //     case 'A':
-    //         printf("Excellent!\n");
-    //         break;
-    //     case 'B':
-    //         printf("Very Good!\n");
-    //         break;
-    //     case 'C':
-    //         printf("Good!\n");
-    //         break;
-    //     case 'D':
-    //         printf("Pass!\n");
-    //         break;
-    //     case 'F':
-    //         printf("Fail!\n");
-    //         break;
-    //     default:
-    //         printf("Invalid grade entered.\n");
-    // }
+    //switch case: grade interpretation
+    switch(grade){
+        case 'A':
+            printf("Excellent!\n");
+            break;
+        case 'B':
+            printf("Very Good!\n");
+            break;
+        case 'C':
+            printf("Good!\n");
+            break;
+        case 'D':
+            printf("Pass!\n");
+            break;
+        case 'F':
+            printf("Fail!\n");
+            break;
+        default:
+            printf("Invalid grade entered.\n");
+    }
 
     //arrays
     int scores[5];

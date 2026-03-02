2560197e8d88ca15d773fa2eb40b747a7a7b7f35
Kyle Noel <khnoel@up.edu.ph>
2026-03-02 16:50:21 +0800

    Demo source code, lecture on pointers.

---
+++ pointers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void change(int *a){
+    *a = 11;
+    printf("inside change: %d\n", *a);
+}
+
+int main() {
+    int x = 10;
+    // int *ptr = &x;
+    //i want to be able to directly change the value of x through the function change()
+    change(&x);
+    printf("in main: %d\n", x);
+    
+    int arr[5] = {1,67,3,4,5};
+    //a pointer is just a variable = a memory address
+    printf("%d", *(arr + 1)); //1
+
+    return 0;
+}

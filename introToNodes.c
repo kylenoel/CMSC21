992b79a8cb5c641146e714c2a533d0f320debc04
Kyle Noel <khnoel@up.edu.ph>
2026-04-23 14:16:55 +0800

    linked list implementation in C (April 23)

--- introToNodes.c
+++ introToNodes.c
@@ -6,19 +6,56 @@ typedef struct Node{
     struct Node *next;
 }Node;
 
+typedef struct LinkedList{
+    Node *head;
+}LinkedList;
+
+void printList(LinkedList *list){
+    Node *current = list->head;
+    while(current != NULL){
+        printf("%d\n", current->data);
+        current = current->next;
+    }
+}
+
+//append
+//insert at position
+//delete at position
+
 int main() {
-    Node *newNode = malloc(sizeof(Node));
-    newNode->data = 10;
-    newNode->next = NULL;
-    
-    printf("%d", newNode->data);
-    
+
+    Node *firstNode = malloc(sizeof(Node));
+    firstNode->data = 10;
+    firstNode->next = NULL;
+
     Node *secondNode = malloc(sizeof(Node));
     secondNode->data = 20;
     secondNode->next = NULL;
-    
-    newNode->next = secondNode;
-    
+
+    firstNode->next = secondNode;
+
+    Node *thirdNode = malloc(sizeof(Node));
+    thirdNode->data = 30;
+    thirdNode->next = NULL;
+    secondNode->next = thirdNode;
+
+
+    // printf("%d\n", firstNode->data);
+    // printf("%d\n", firstNode->next->data);
+
+    LinkedList l; //{10, 20, 30}
+    l.head = firstNode; 
+
+    printList(&l);
+
+    Node *fourthNode = malloc(sizeof(Node));
+    scanf("%d", &fourthNode->data);
+
+    thirdNode->next = fourthNode;
+    fourthNode->next = NULL;
+
+    printList(&l);
+
 
     return 0;
 }
